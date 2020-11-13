3d0ea3edafe731c9745cb7444f34bb1246280816
ShahzaibSE <shahzaibanalyst@outlook.com>
2020-11-14 01:30:51 +0500

    Test cases added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,25 @@
-import Enzyme from 'enzyme';
-import ReactSixteenAdapter from 'enzyme-adapter-react-16';
-//
-Enzyme.configure({ adapter: new ReactSixteenAdapter() });
+import React from "react"
+import {shallow, mount} from "enzyme"
+// Component.
+import GridComponent from "./components/Grid/GridComponent"
+import App from "./App"
+
+describe("App root component", ()=>{
+    let container: any;
+
+    beforeEach(()=>(
+        container = shallow(<App/>)
+    ))
+
+    it("testing UI of <App/>", ()=>{
+        expect(container).toMatchSnapshot()
+    })
+
+    it("at least one div to be found", ()=>{
+        expect(container.find("div").length).toEqual(1)
+    })
+
+    it("check if Grid component has been rendered", ()=>{
+        expect(container.containsMatchingElement(<GridComponent/>)).toEqual(true)
+    })
+})
