e8096e400850e325937e8c4a15ad349069edc191
ShahzaibSE <shahzaibanalyst@outlook.com>
2020-11-18 22:21:06 +0500

    Updated

--- src/components/ProductList/ProductList.container.component.tsx
+++ src/components/ProductList/ProductList.container.component.tsx
@@ -5,11 +5,21 @@ import Grid from "@material-ui/core/Grid";
 // Assets.
 import "./ProductList.css";
 // Component.
-import ProductCardComponent from "../ProductCard/ProductCard.component";
+import ProductListComponent from "./ProductList.component";
 // Selector from slice.
 import {productListSelector, fetchProducts} from "../slices/product.slice";
 
 const ProductListContainerComponent = () => {
+    const dispatch = useDispatch()
+    const {products} = useSelector(productListSelector)
+    const [productList, setProductList] = useState([])
+
+    useEffect(()=>{
+        dispatch(fetchProducts())
+        console.log("<ProductListComponent> - Products")
+        console.log(products)
+    },[])
+
     return (
         <div>
             
