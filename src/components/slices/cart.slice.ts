ac0b710f273ca373c494fbbc6f841c6c046a8c23
ShahzaibSE <shahzaibanalyst@outlook.com>
2020-11-21 23:25:08 +0500

    Cart Slice updated

--- src/components/slices/cart.slice.ts
+++ src/components/slices/cart.slice.ts
@@ -10,14 +10,35 @@ export const cartSlice = createSlice({
            state.cart_items = action.payload
        },
        add_cart_items: (state:any, action:any)=>{
-           state.cart_items.push(action.payload)
-        //    state.cart_items.map((cart_item:any) => {
-        //        if(cart_item)
-        //    })
+           state.cart_items = [action.payload, ...state.cart_items]
        },
-       remove_cart_items: (state:any, action: any)=>{
-
+       remove_cart_items: (state:any, action:any)=>{
+        state.cart_items.filter( (cart_item:any) => cart_item.id !== action.payload)
        }
+    //    add_cart_items: (state, action) => {
+    //     return state.cart_items.map((cart_item:any) => {
+    //       if (cart_item.id !== action.payload.id) {
+    //         return cart_item
+    //       }
+  
+    //       return {
+    //         ...cart_item,
+    //         added: true
+    //       }
+    //     })
+    //   },
+    //   remove_cart_item: (state, action) => {
+    //     return state.cart_items.map((cart_item:any) => {
+    //       if (cart_item.id !== action.payload.id) {
+    //         return cart_item
+    //       }
+  
+    //       return {
+    //         ...cart_item,
+    //         added: false
+    //       }
+    //     })
+    //   }
     }
 })
 
